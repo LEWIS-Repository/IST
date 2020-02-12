2ec612b9f39be0571776251ebf56ccb888542978
nmlewis1913 <nmlewis@nemcc.edu>
2020-02-12 08:56:44 -0800

    Add files via upload

---
+++ IST/IST/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  IST
+//
+//  Created by IST on 2/12/20.
+//  Copyright © 2020 IST. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IST/IST/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  IST
+//
+//  Created by IST on 2/12/20.
+//  Copyright © 2020 IST. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
